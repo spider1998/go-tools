8d1d29e1ad80d9266419f70e476d14e2a30b2b93
刘凡 <liuf@sdkeji.com>
2019-04-02 15:50:08 +0800

    增加生成随机字符串

--- string.go
+++ string.go
@@ -1,9 +1,21 @@
 package routing
 
+import "math/rand"
+
 func ReverseStr(from string) string {
 	runes := []rune(from)
 	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
 		runes[i], runes[j] = runes[j], runes[i]
 	}
 	return string(runes)
+}
+
+const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func RandString(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
 }

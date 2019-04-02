aaef1125f723ffa4a4c5017f57950593102df717
刘凡 <liuf@sdkeji.com>
2019-04-02 15:39:12 +0800

    文件操作

---
+++ file.go
@@ -0,0 +1,29 @@
+package routing
+
+import (
+	"os"
+
+	"github.com/pkg/errors"
+)
+
+func MakeDirectory(path string) error {
+	fi, err := os.Stat(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return os.MkdirAll(path, 0x755)
+		}
+		return err
+	}
+	if !fi.IsDir() {
+		return errors.New("specified path is not a directory")
+	}
+	return nil
+}
+
+func PathExist(path string) bool {
+	_, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+	return true
+}

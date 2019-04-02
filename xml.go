6a46d5722e860a3e8ae12b40549611533fb1af41
刘凡 <liuf@sdkeji.com>
2019-04-02 15:52:46 +0800

    xml-map

---
+++ xml.go
@@ -0,0 +1,36 @@
+package routing
+
+import (
+	"encoding/xml"
+	"io"
+)
+
+func XMLToMap(reader io.Reader, ignoreFirst bool) (map[string]string, error) {
+	params := make(map[string]string)
+	d := xml.NewDecoder(reader)
+	value := ""
+	for {
+		token, err := d.Token()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return nil, err
+		}
+		switch t := token.(type) {
+		case xml.StartElement:
+			if ignoreFirst {
+				ignoreFirst = false
+				continue
+			}
+			value = t.Name.Local
+		case xml.CharData:
+			if value != "" {
+				params[value] = string(t)
+			}
+		case xml.EndElement:
+			value = ""
+		}
+	}
+	return params, nil
+}

6404c953e3463a9fefe2b17a7de588ed4c61ca7d
刘凡 <liuf@sdkeji.com>
2019-04-02 15:34:07 +0800

    hash密码生成及对比

---
+++ hash-psw.go
@@ -0,0 +1,17 @@
+package routing
+
+import "golang.org/x/crypto/bcrypt"
+
+const DefaultPasswordCost = 10
+
+func GeneratePasswordHash(input []byte, costs ...int) ([]byte, error) {
+	cost := DefaultPasswordCost
+	if len(costs) > 0 {
+		cost = costs[0]
+	}
+	return bcrypt.GenerateFromPassword(input, cost)
+}
+
+func ValidatePassword(input, hash []byte) error {
+	return bcrypt.CompareHashAndPassword(hash, input)
+}
